8796c84d352055256c85896cbe43caab576c1822
Jeffery McLean <jeffery.mclean@gmail.com>
2013-12-15 01:48:33 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marbles::Application.config.secret_key_base = '76ae5867278f5c918c9fd26c9b3e516b2ba14ea4b34872012aca672399d78f54e56f4aef13a35793171f45e10a256917c3a7d1ee1e2d4a384948c292f6eee306'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Marbles::Application.config.session_store :cookie_store, key: '_marbles_session'
