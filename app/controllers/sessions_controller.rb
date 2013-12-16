d1c7684202bfbc54d34e56109d71139a84be0816
Jeffery McLean <jeffery.mclean@gmail.com>
2013-12-16 20:19:56 +0000

    css styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,6 @@ class SessionsController < ApplicationController
     
   def create
     user = User.find_by(email: params[:email])
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to users_path, notice: "Welcome back, #{user.username}!" 
