e167124e4953c4a11f8d43c705133792c6feb629
Jeffery McLean <jeffery.mclean@gmail.com>
2013-12-18 04:04:31 +0000

    styling background and marble number logic

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -4,11 +4,14 @@ class ExchangesController < ApplicationController
     username = params[:username] 
     user = User.where(username: username).first
     
-    if user.present?
+    if user.present? && current_user.marbles > 0 
       user.marbles += 1
       user.save
       current_user.marbles -= 1
       current_user.save
+      flash[:alert] = "You successfully transferred a marble to #{user.username}. Great job #{current_user.username}!"
+    else flash[:alert] = "That user is not present OR you don't have any marbles to give. Which is it?"
     end
+    redirect_to users_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   def index
   end
 
-
   def new
     @user = User.new
   end
