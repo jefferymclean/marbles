9a6cb692bf26b263ac6a2a89e7191bd0e1f5f40c
Jeffery McLean <jeffery.mclean@gmail.com>
2013-12-18 06:10:44 +0000

    styling and numbers and words gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   protected 
 
@@ -13,7 +13,6 @@ class ApplicationController < ActionController::Base
 
   def restrict_access
     if !current_user 
-      flash[:alert] = "You must log in."
       redirect_to new_session_path
     end
   end
