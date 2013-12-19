a7bbd40531d134d7ee79dc703786ea8165e6b210
Jeffery McLean <jeffery.mclean@gmail.com>
2013-12-19 09:22:17 +0000

    formatting

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -9,8 +9,8 @@ class ExchangesController < ApplicationController
       user.save
       current_user.marbles -= 1
       current_user.save
-      flash[:alert] = "You successfully transferred a marble to #{user.username}. Great job #{current_user.username}!"
-    else flash[:alert] = "That user is not present OR you don't have any marbles to give. Which is it?"
+      flash[:alert] = "Marble Transferred to #{user.username}"
+    else flash[:alert] = "That user isnt present OR you dont have any marbles to give. Which is it?"
     end
     redirect_to users_path
   end
